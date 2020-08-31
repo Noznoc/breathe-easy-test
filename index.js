828dd30cd467367597a1b354af74ffe90a9df73d
Noznoc <juliaconzon@gmail.com>
2020-08-31 07:53:57 -0400

    testing for CORS issue

--- index.js
+++ index.js
@@ -3,157 +3,103 @@ init();
 
 function init() {
 	
-	// Initialize value for embedded Excel document.
-	var ewa = null;
-
-	// Add event handler for onload event.
-	if (window.attachEvent) { 
-			window.attachEvent("onload", ewaOnPageLoad);    
-	} else { 
-			window.addEventListener("DOMContentLoaded", ewaOnPageLoad, false); 
-	}
+	d3.csv('data_DUMMY.csv', function(data){
+		console.log(data)
+	});
 	
-	// Add event handler for applicationReady event.
-	function ewaOnPageLoad() {
-			if (typeof (Ewa) != "undefined") {
-				Ewa.EwaControl.add_applicationReady(ewaApplicationReady);
-			} else {
-				alert("Error - the EWA is not loaded.");
-			}
-	}
-
-	function ewaApplicationReady() {
-			// Get a reference to the Excel Services web part.
-			ewa = Ewa.EwaControl.getInstances().getItem(0);
-			getData();
-	}
+	buildMap(features)
 	
-	function getData() {    
-		// Get range for entire Data sheet
-		// TO DO: FIGURE OUT HOW TO GET THE ROW LENGTH SHOULD NOT HARD CODE (JULIA)
-		// var rows = ewa.getActiveWorkbook().getActiveSheet().getRowCount();
-		// console.log(test);
-		// The getRange() function parameters: first row, first column, last row, last column. First starts at 0. 
-		var range = ewa.getActiveWorkbook().getActiveSheet().getRange(1,0,6,23); // IF MORE DATA IS ADDED, CHANGE 4 TO WHATEVER AMOUNT OF ROWS THERE IS!
-		
-		// Get values from range.
-		range.getValuesAsync(0,buildMap,range);
-	}
-	
-	function buildMap(asyncResult) {
-			// Get the value from asyncResult if the asynchronous operation was successful.
-			if (asyncResult.getCode() == 0) {
-				
-				// Get the array of range values from asyncResult.
-				var values = asyncResult.getReturnValue(),
-						test = asyncResult.getCode(),
-						features = [];
-					console.log(test)
-				// Loop through the array of range values and prepare each row into a feature. Each feature will then be added to an array of features that will be added into the map as a data source.
-				for (var i = 0; i < values.length; i++) {
-					 // TO DO: ADD OTHER DATA TO FEATURE FOR MAP (MELISSA)
-					 // Essentially add additional keys and values to the "properties" object. 
-					 // The following must be the structure of the object: "Column Name / Whatever you want to call it / Text to appear on the Map":"'+values[i][enter column number starting at 0 as column #1]+'".
-					 // TO DO: Add B-K columns
-					 // To DO: View data as table
-					 console.log(values[i][23])
-					 var feature = '{"type": "Feature","properties": {"Location": "' + values[i][22] + '", "Time": "' + new Date(values[i][0]) + '", "Quality": "'+ aqi(values[i][19]) +'", "Temp": "' + values[i][1] + '", "Humidity": "' + values[i][2] + '", "Particulate Matter 10.0 (ug/m^3)": "' + values[i][5] + '", "Ozone (ppb)": "' + values[i][6] + '"},"geometry": {"type": "Point","coordinates": ['+values[i][12]+','+values[i][11]+']}}';
-					 features.push(JSON.parse(feature));
-				}
+	function buildMap(features) {
 
-				// Create a map. Will have to add an accessToken that is specific for this project.
-				// TO DO: FIGURE OUT MAP TOKEN (JULIA)
-				mapboxgl.accessToken = 'pk.eyJ1IjoianVsY29ueiIsImEiOiJja2N0NGU3eGkwZGtqMnJxbGM0dXM4am50In0.mlekGyVVwR95ATKGkcISOg';
-				var map = new mapboxgl.Map({
-					container: 'map',
-					style: 'mapbox://styles/mapbox/streets-v11',
-					center: [-75.68912, 45.40914],
-					zoom: 5,
-					maxZoom: 14,
-					minZoom: 10,
-					hash: true,
-					maxBounds: [-168.39312,40.713956,-50.971241,83.359511] // TO DO: UPDATE THIS WITH JUST OTTAWA BOUNDS, NOT CANADA (JULIA/MELISSA)
+			// Create a map. Will have to add an accessToken that is specific for this project.
+			// TO DO: FIGURE OUT MAP TOKEN (JULIA)
+			mapboxgl.accessToken = 'pk.eyJ1IjoianVsY29ueiIsImEiOiJja2N0NGU3eGkwZGtqMnJxbGM0dXM4am50In0.mlekGyVVwR95ATKGkcISOg';
+			var map = new mapboxgl.Map({
+				container: 'map',
+				style: 'mapbox://styles/mapbox/streets-v11',
+				center: [-75.68912, 45.40914],
+				zoom: 5,
+				maxZoom: 14,
+				minZoom: 10,
+				hash: true,
+				maxBounds: [-168.39312,40.713956,-50.971241,83.359511] // TO DO: UPDATE THIS WITH JUST OTTAWA BOUNDS, NOT CANADA (JULIA/MELISSA)
+			});
+			
+			map.on('load', function(){
+				console.log(features)
+				// Create data source to store the features that were collected from the Excel spreadsheet.
+				map.addSource('data', {
+					'type': 'geojson',
+					'data': {
+						'type': 'FeatureCollection',
+						'features': features
+					}
 				});
 				
-				map.on('load', function(){
-					console.log(features)
-					// Create data source to store the features that were collected from the Excel spreadsheet.
-					map.addSource('data', {
-						'type': 'geojson',
-						'data': {
-							'type': 'FeatureCollection',
-							'features': features
+				// Add a new map layer from the above data source. This layer represents the sites.
+				map.addLayer({
+						'id': 'sites',
+						'type': 'circle',
+						'source': 'data',
+						'paint': { // TO DO: ADD LOGIC TO MAKE POINTS COLOURED BASED ON A VALUE. TO BE DISCUSSED B/W MELISSA AND JULIA.
+							'circle-radius': 10, // change size of circle
+							'circle-stroke-width': 0.3, // outline colour
+							'circle-stroke-color': '#ccc',
+							'circle-color': [
+								'match',
+								['get', 'Quality'],
+								'Low Risk',
+								'#00C851',
+								'High Risk',
+								'#ff4444',
+								/* other */ '#ccc'
+							]
 						}
 					});
-					
-					// Add a new map layer from the above data source. This layer represents the sites.
-					map.addLayer({
-							'id': 'sites',
-							'type': 'circle',
-							'source': 'data',
-							'paint': { // TO DO: ADD LOGIC TO MAKE POINTS COLOURED BASED ON A VALUE. TO BE DISCUSSED B/W MELISSA AND JULIA.
-								'circle-radius': 12,
-								'circle-stroke-width': 0.3,
-								'circle-stroke-color': '#ccc',
-								'circle-color': [
-									'match',
-									['get', 'Quality'],
-									'Low Risk',
-									'#00C851',
-									'High Risk',
-									'#ff4444',
-									/* other */ '#ccc'
-								]
-							}
-						});
-				});
-				
-				// Initialize a popup for the map.
-				var popup;
+			});
+			
+			// Initialize a popup for the map.
+			var popup;
 
-				// Create hover events so that when a user hover's over point (mouseenter), the popup appears.
-				map.on('mouseenter', 'sites', function(e) {
-					var text,
-						content;
-						
-					map.getCanvas().style.cursor = 'pointer';
-				
-					// Set popup text.
-					content = '<h2>' + e.features[0].properties.Location + '</h2>';
+			// Create hover events so that when a user hover's over point (mouseenter), the popup appears.
+			map.on('mouseenter', 'sites', function(e) {
+				var text,
+					content;
 					
-					// Depending on the air quality type, make the colours of the popup different.
-					if (e.features[0].properties.Quality == 'Low Risk') {
-						content += '<h3 style="background-color: #00C851;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
-					} else if (e.features[0].properties.Quality == 'High Risk') {
-						content += '<h3 style="background-color: #ff4444;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
-					} else {
-						content += '<h3 style="background-color: #ccc;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
-					}
-					
-					content += buildTable([e.features[0].properties.Time.toString(), e.features[0].properties.Temp, e.features[0].properties.Humidity],['Date', 'Temperature (C)', 'Humidity (%)']);
-
-					popup = new mapboxgl.Popup({
-						 closeButton: false,
-						 closeOnClick: false,
-						 className: e.features[0].properties.Quality.split(' ').join('_').toLowerCase()
-					});
-
-					// Populate popup content.
-					popup.setLngLat(e.features[0].geometry.coordinates)
-						.setHTML(content)
-						.addTo(map);
-				});
+				map.getCanvas().style.cursor = 'pointer';
+			
+				// Set popup text.
+				content = '<h2>' + e.features[0].properties.Location + '</h2>';
 				
-				// And when a user leaves a point (mouseleave), the popup disappears.
-				map.on('mouseleave', 'sites', function(e) {
-					map.getCanvas().style.cursor = '';
-					popup.remove();
-				});
+				// Depending on the air quality type, make the colours of the popup different.
+				if (e.features[0].properties.Quality == 'Low Risk') {
+					content += '<h3 style="background-color: #00C851;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
+				} else if (e.features[0].properties.Quality == 'High Risk') {
+					content += '<h3 style="background-color: #ff4444;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
+				} else {
+					content += '<h3 style="background-color: #ccc;">' + e.features[0].properties.Quality.toUpperCase() + '</h3>';
+				}
 				
-			} else {
-				alert('Operation failed with error message ' + asyncResult.getDescription() + '.');
-			}    
-	}
+				content += buildTable([e.features[0].properties.Time.toString(), e.features[0].properties.Temp, e.features[0].properties.Humidity],['Date', 'Temperature (C)', 'Humidity (%)']);
+
+				popup = new mapboxgl.Popup({
+					 closeButton: false,
+					 closeOnClick: false,
+					 className: e.features[0].properties.Quality.split(' ').join('_').toLowerCase()
+				});
+
+				// Populate popup content.
+				popup.setLngLat(e.features[0].geometry.coordinates)
+					.setHTML(content)
+					.addTo(map);
+			});
+			
+			// And when a user leaves a point (mouseleave), the popup disappears.
+			map.on('mouseleave', 'sites', function(e) {
+				map.getCanvas().style.cursor = '';
+				popup.remove();
+			});
+	}   
 }
 
 function aqi(val) {
